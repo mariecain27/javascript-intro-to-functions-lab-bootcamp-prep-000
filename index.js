1122c14db17805c03a42e3476db951fa17911805
mariecain27@gmail.com <mariecain27@gmail.com>
2018-05-03 04:24:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function sayHiToGrandma(string) {
   } else if (string.toUpperCase === string) {
     return ("YES INDEED!")
   } else {
-    return ("I love you, too.")
+    return ("YES INDEED!")
   }
 }
