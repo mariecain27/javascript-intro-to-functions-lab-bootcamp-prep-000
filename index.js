ecd3f58d1d445c235ff4c33335207ec2c76a6fd2
mariecain27@gmail.com <mariecain27@gmail.com>
2018-05-03 03:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string)
+  console.log(string).toUpperCase()
 }
-
-logShout("hello").toUpperCase()
-
 
 
